4928a51faf9f8c0996339c48a22a6d7d4626a263
Darsh_Khetan <khetandarsh@gmail.com>
2024-09-19 17:14:55 +0530

    .

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -27,13 +27,13 @@ const Footer = () => {
         <div className="footer-content-right">
             <h2>GET IN TOUCH</h2>
             <ul>
-                <li>+1-212-456-7890</li>
-                <li>contact@tomato.com</li>
+                <li>+917619496052</li>
+                <li>ovenDelight@.com</li>
             </ul>
         </div>
       </div>
       <hr />
-      <p className="footer-copyright">Copyright 2024 © Tomato.com - All Right Reserved.</p>
+      <p className="footer-copyright">Copyright 2024 © OvenDelight.com - All Right Reserved.</p>
     </div>
   )
 }
